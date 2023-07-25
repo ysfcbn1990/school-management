5fec9f71c9de35c7bdc9a33e0dca2fe88d188dd3
YusufCoban <ysfcbn49@gmail.com>
2023-07-25 23:08:38 +0300

    security last code

--- src/main/java/com/schoolmanagement/security/jwt/JwtUtils.java
+++ src/main/java/com/schoolmanagement/security/jwt/JwtUtils.java
@@ -1,50 +1,87 @@
 package com.schoolmanagement.security.jwt;
 
 import com.schoolmanagement.security.service.UserDetailsImpl;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
+@Component
 public class JwtUtils {
 
-    private static  final Logger LOGGER =LoggerFactory.getLogger(JwtUtils.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(JwtUtils.class);
 
     @Value("${backendapi.app.jwtExpirationMs}")
-    private long jwtExpirationMs;
+    private long jwtExpirationMs ;
 
     @Value("${backendapi.app.jwtSecret}")
     private String jwtSecret;
 
-    //Not: Generate JWT****************************************
+    // Not: Generate JWT *************************************************
     public String generateJwtToken(Authentication authentication){
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-
         return generateTokenFromUsername(userDetails.getUsername());
+
     }
 
     public String generateTokenFromUsername(String username){
-
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(new Date().getTime()+jwtExpirationMs))
-                .signWith(SignatureAlgorithm.HS512,jwtSecret)
+                .setExpiration(new Date(new Date().getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
                 .compact();
-
-
     }
 
 
+    // Not : Validate JWT *************************************************
+    public boolean validateJwtToken(String jwtToken ){
+
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(jwtToken);
+            return true;
+        } catch (ExpiredJwtException e) {
+            LOGGER.error("Jwt token is expired : {}", e.getMessage() );
+        } catch (UnsupportedJwtException e) {
+            LOGGER.error("Jwt token is unsupported : {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+            LOGGER.error("Jwt token is invalid : {}", e.getMessage());
+        } catch (SignatureException e) {
+            LOGGER.error("Jwt Signature is invalid : {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            LOGGER.error("Jwt is empty : {}", e.getMessage());
+        }
+
+        return false;
+    }
 
 
+    // Not: getUsernameFromJWT ********************************************
+    public String getUserNameFromJwtToken(String token){
 
-
-    //Not:Validate JWT*****************************************
-
-    //Not: getUsernameFromJwt
+        return Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
